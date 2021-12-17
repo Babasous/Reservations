fc8fee0df58b3b15705c8cbde04a761cf892a50b
Alain <albulaya@gmail.com>
2021-12-17 03:00:55 +0100

    Tmplate Locality

--- resources/views/locality/show.blade.php
+++ resources/views/locality/show.blade.php
@@ -1,8 +1,13 @@
 @extends('layouts.app')
 
-@section('title', 'Fiche d\'une localité')
+@section('title', 'Liste des localités')
 
 @section('content')
     <h1>{{ $locality->postal_code }} {{ ucfirst($locality->locality) }}</h1>
+    <ul>
+        @foreach ($locality->locations as $location)
+           <li>{{$location->designation}}</li> 
+        @endforeach
+    </ul>
     <nav><a href="{{ route('locality_index') }}">Retour à l'index</a></nav>
 @endsection
