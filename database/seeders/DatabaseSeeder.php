9673be9a022fea259fb24f8e51d52b3024168190
Alain <albulaya@gmail.com>
2021-12-21 21:35:02 +0100

    Update templates Show (added artists' list

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             ShowSeeder::class,
             RepresentationSeeder::class,
             ArtistTypeSeeder::class,
+            ArtistTypeShowSeeder::class,
         ]);
 
         // \App\Models\User::factory(10)->create();
